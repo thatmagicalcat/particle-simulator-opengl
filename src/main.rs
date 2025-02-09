772e6abb2042e2cf47d34bda7731a0df1cf602f8
thatmagicalcat <thatmagicalcat@duck.com>
2025-02-09 23:04:25 +0530

    migrated gl -> glow

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,9 @@ mod shader;
 mod systems;
 
 use components::*;
-use systems as sys;
-
+use glow::HasContext;
 use shader::Shader;
+use systems as sys;
 
 use legion::*;
 
@@ -33,13 +33,7 @@ fn main() {
         .add_system(sys::check_wall_collision_system())
         .build();
 
-    world.push((
-        EntityIndex(0),
-        Velocity(glam::vec2(30.0, 30.0)),
-        Mass(100.0),
-    ));
-
-    resources.insert(InstanceCount(1));
+    resources.insert(InstanceCount(0));
 
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
 
@@ -53,133 +47,135 @@ fn main() {
 
     resources.insert(window.get_size());
 
+    window.set_cursor_pos_polling(true);
     window.set_key_polling(true);
+    window.set_mouse_button_polling(true);
+
     window.make_current();
 
-    gl::load_with(|s| window.get_proc_address(s));
+    // gl::load_with(|s| window.get_proc_address(s));
+    let gl =
+        unsafe { glow::Context::from_loader_function(|s| window.get_proc_address(s) as *const _) };
 
-    glfw.set_swap_interval(glfw::SwapInterval::None);
-    unsafe { gl::Viewport(0, 0, window.get_size().0, window.get_size().1) };
+    glfw.set_swap_interval(glfw::SwapInterval::Adaptive);
+    unsafe { gl.viewport(0, 0, window.get_size().0, window.get_size().1) };
     window.set_size_polling(true);
 
-    let (mut vao, mut vbo, mut ebo) = (0, 0, 0);
+    let (vao, vbo, ebo);
 
     let (vertices, indices) = generate_circle(POINT_COUNT as _);
 
     unsafe {
-        gl::GenVertexArrays(1, &raw mut vao);
-        gl::GenBuffers(1, &raw mut vbo);
-        gl::GenBuffers(1, &raw mut ebo);
+        vao = gl.create_vertex_array().unwrap();
+        vbo = gl.create_buffer().unwrap();
+        ebo = gl.create_buffer().unwrap();
 
-        gl::BindVertexArray(vao);
+        gl.bind_vertex_array(Some(vao));
 
-        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
-        gl::BindBuffer(gl::ELEMENT_ARRAY_BUFFER, ebo);
+        gl.bind_buffer(glow::ARRAY_BUFFER, Some(vbo));
+        gl.bind_buffer(glow::ELEMENT_ARRAY_BUFFER, Some(ebo));
 
-        gl::BufferData(
-            gl::ARRAY_BUFFER,
-            (vertices.len() * std::mem::size_of::<f32>()) as isize,
-            vertices.as_ptr() as _,
-            gl::STATIC_DRAW,
+        gl.buffer_data_u8_slice(
+            glow::ARRAY_BUFFER,
+            std::slice::from_raw_parts(
+                vertices.as_ptr() as _,
+                vertices.len() * std::mem::size_of::<f32>(),
+            ),
+            glow::STATIC_DRAW,
         );
 
-        gl::BufferData(
-            gl::ELEMENT_ARRAY_BUFFER,
-            (indices.len() * std::mem::size_of::<u32>()) as isize,
-            indices.as_ptr() as _,
-            gl::STATIC_READ,
+        gl.buffer_data_u8_slice(
+            glow::ELEMENT_ARRAY_BUFFER,
+            std::slice::from_raw_parts(indices.as_ptr() as _, indices.len() * std::mem::size_of::<f32>()),
+            glow::STATIC_READ,
         );
 
-        gl::EnableVertexAttribArray(0);
-        gl::VertexAttribPointer(
+        gl.enable_vertex_attrib_array(0);
+        gl.vertex_attrib_pointer_f32(
             0,
             2,
-            gl::FLOAT,
-            gl::FALSE,
+            glow::FLOAT,
+            false,
             2 * std::mem::size_of::<f32>() as i32,
-            std::ptr::null(),
+            0,
         );
 
         // still bound
     }
 
     // instancing
-    let mut instance_vbo = 0;
-    let instance_data = [100.0, 100.0, 100.0];
+    let instance_vbo;
     let instance_data_ptr;
+    let mut instance_data_offset = 0;
 
     unsafe {
-        gl::GenBuffers(1, &raw mut instance_vbo);
-        gl::BindBuffer(gl::ARRAY_BUFFER, instance_vbo);
+        instance_vbo = gl.create_buffer().unwrap();
+        gl.bind_buffer(glow::ARRAY_BUFFER, Some(instance_vbo));
 
-        gl::BufferStorage(
-            gl::ARRAY_BUFFER,
+        gl.buffer_storage(
+            glow::ARRAY_BUFFER,
             INITIAL_BUFFER_SIZE as _,
-            std::ptr::null(),
-            gl::MAP_WRITE_BIT | gl::MAP_PERSISTENT_BIT | gl::MAP_COHERENT_BIT,
+            None,
+            glow::MAP_WRITE_BIT | glow::MAP_PERSISTENT_BIT | glow::MAP_COHERENT_BIT,
         );
 
-        instance_data_ptr = gl::MapBufferRange(
-            gl::ARRAY_BUFFER,
+        instance_data_ptr = gl.map_buffer_range(
+            glow::ARRAY_BUFFER,
             0,
             INITIAL_BUFFER_SIZE as _,
-            gl::MAP_WRITE_BIT | gl::MAP_PERSISTENT_BIT | gl::MAP_COHERENT_BIT,
+            glow::MAP_WRITE_BIT | glow::MAP_PERSISTENT_BIT | glow::MAP_COHERENT_BIT,
         ) as *mut f32;
-
-        std::ptr::copy_nonoverlapping(
-            instance_data.as_ptr(),
-            instance_data_ptr,
-            instance_data.len(),
-        );
 
         resources.insert(InstanceDataPtr::new(instance_data_ptr));
 
         let radius_offset = std::mem::size_of::<f32>() * 2;
 
         // position
-        gl::EnableVertexAttribArray(1);
-        gl::VertexAttribDivisor(1, 1);
-        gl::VertexAttribPointer(
-            1,
-            2,
-            gl::FLOAT,
-            gl::FALSE,
-            INSTANCE_DATA_STRIDE as _,
-            0 as _,
-        );
+        gl.enable_vertex_attrib_array(1);
+        gl.vertex_attrib_divisor(1, 1);
+        gl.vertex_attrib_pointer_f32(1, 2, glow::FLOAT, false, INSTANCE_DATA_STRIDE as _, 0);
 
         // radius
-        gl::EnableVertexAttribArray(2);
-        gl::VertexAttribDivisor(2, 1);
-        gl::VertexAttribPointer(
+        gl.enable_vertex_attrib_array(2);
+        gl.vertex_attrib_divisor(2, 1);
+        gl.vertex_attrib_pointer_f32(
             2,
             1,
-            gl::FLOAT,
-            gl::FALSE,
+            glow::FLOAT,
+            false,
             INSTANCE_DATA_STRIDE as _,
             radius_offset as _,
         );
 
         // unbind
-        gl::BindVertexArray(0);
+        gl.bind_vertex_array(None);
     }
 
-    let shader = Shader::from_str(include_str!("shader.glsl"), "vertex", "fragment")
+    let shader = Shader::from_str(&gl, include_str!("shader.glsl"), "vertex", "fragment")
         .expect("Failed to load shader");
 
     let orthographic_uniform = |(width, height)| unsafe {
         shader.use_shader();
-        gl::UniformMatrix4fv(
-            shader.get_uniform_location("ortho").unwrap(),
-            1,
-            gl::FALSE,
-            glam::Mat4::orthographic_rh_gl(0.0, width as _, height as _, 0.0, -1.0, 1.0)
-                .to_cols_array()
-                .as_ptr(),
+        gl.uniform_matrix_4_f32_slice(
+            Some(&shader.get_uniform_location("ortho").unwrap()),
+            false,
+            &glam::Mat4::orthographic_rh_gl(0.0, width as _, height as _, 0.0, -1.0, 1.0)
+                .to_cols_array(),
         );
     };
 
     orthographic_uniform(window.get_size());
+
+    unsafe {
+        std::ptr::copy_nonoverlapping(
+            [100.0, 100.0, 20.0, 300.0, 300.0, 30.0].as_ptr(),
+            instance_data_ptr,
+            6,
+        )
+    };
+
+    let mut mouse_down = false;
+    let particle_radius: f32 = 3.0;
 
     let mut clock = Instant::now();
     while !window.should_close() {
@@ -195,28 +191,60 @@ fn main() {
             }
 
             WindowEvent::Size(width, height) => {
-                unsafe { gl::Viewport(0, 0, width, height) };
+                unsafe { gl.viewport(0, 0, width, height) };
                 orthographic_uniform((width, height));
                 resources.insert(window.get_size());
+            }
+
+            WindowEvent::MouseButton(glfw::MouseButtonLeft, glfw::Action::Press, _) => {
+                mouse_down = true
+            }
+            WindowEvent::MouseButton(glfw::MouseButtonLeft, glfw::Action::Release, _) => {
+                mouse_down = false
             }
 
             _ => {}
         });
 
+        if mouse_down && instance_data_offset < 50_000 {
+            let v_x: f32 = rand::random_range(-30.0..30.0);
+            let v_y: f32 = rand::random_range(-30.0..30.0);
+
+            let (x, y) = window.get_cursor_pos();
+
+            unsafe {
+                std::ptr::copy_nonoverlapping(
+                    [x as _, y as _, particle_radius].as_ptr(),
+                    instance_data_ptr.add(instance_data_offset),
+                    3,
+                );
+
+                instance_data_offset += 3;
+            };
+
+            world.push((
+                EntityIndex(resources.get::<InstanceCount>().unwrap().0 as _),
+                Velocity(glam::vec2(v_x, v_y)),
+                Mass(particle_radius.powi(2)),
+            ));
+
+            resources.get_mut::<InstanceCount>().unwrap().0 += 1;
+        }
+
         resources.insert(DeltaTime(dt));
         schedule.execute(&mut world, &mut resources);
 
         unsafe {
-            gl::ClearColor(0.2, 0.2, 0.2, 1.0);
-            gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl.clear_color(0.01, 0.01, 0.01, 1.0);
+            gl.clear(glow::COLOR_BUFFER_BIT);
 
             shader.use_shader();
 
-            gl::BindVertexArray(vao);
-            gl::DrawElementsInstanced(
-                gl::TRIANGLES,
+            gl.bind_vertex_array(Some(vao));
+            gl.draw_elements_instanced(
+                glow::TRIANGLES,
                 indices.len() as _,
-                gl::UNSIGNED_INT,
+                glow::UNSIGNED_INT,
                 0 as _,
                 resources.get::<InstanceCount>().unwrap().0,
             );
--- src/shader.rs
+++ src/shader.rs
@@ -1,34 +1,22 @@
 #![allow(unused)]
 
-macro_rules! uniform {
-    [ $fn_name:tt, $rust_ty:ty, $uniform_fn:ident] => {
-        pub fn $fn_name(&self, name: &str, value: $rust_ty) {
-            let binding = std::ffi::CString::new(name)
-                .expect(concat!("CString::new failed in Shader::set_", stringify!($fn_name)));
+use glow::HasContext;
 
-            unsafe {
-                gl::$uniform_fn(
-                    gl::GetUniformLocation(self.0, binding.as_ptr() as _),
-                    value as _,
-                )
-            };
-        }
-    };
-}
+pub struct Shader<'a>(&'a glow::Context, glow::NativeProgram);
 
-pub struct Shader(u32);
-
-impl Shader {
+impl<'a> Shader<'a> {
     pub fn from_file(
+        gl: &'a glow::Context,
         path: &str,
         vertex_section: &str,
         fragment_section: &str,
     ) -> std::io::Result<Self> {
         let contents = std::fs::read_to_string(path)?;
-        Self::from_str(&contents, vertex_section, fragment_section)
+        Self::from_str(gl, &contents, vertex_section, fragment_section)
     }
 
     pub fn from_str(
+        gl: &'a glow::Context,
         contents: &str,
         vertex_section: &str,
         fragment_section: &str,
@@ -43,51 +31,41 @@ impl Shader {
             .get(fragment_section)
             .ok_or(std::io::ErrorKind::NotFound)?;
 
-        let vert_shader = compile_shader(gl::VERTEX_SHADER, vert);
-        let frag_shader = compile_shader(gl::FRAGMENT_SHADER, frag);
+        let vert_shader = compile_shader(gl, glow::VERTEX_SHADER, vert);
+        let frag_shader = compile_shader(gl, glow::FRAGMENT_SHADER, frag);
 
-        let program = unsafe { gl::CreateProgram() };
+        let program = unsafe { gl.create_program().unwrap() };
 
         unsafe {
-            gl::AttachShader(program, vert_shader);
-            gl::AttachShader(program, frag_shader);
-            gl::LinkProgram(program);
-            gl::ValidateProgram(program);
+            gl.attach_shader(program, vert_shader);
+            gl.attach_shader(program, frag_shader);
+            gl.link_program(program);
+            gl.validate_program(program);
 
             // cleanup
-            gl::DeleteShader(vert_shader);
-            gl::DeleteShader(frag_shader);
+            gl.delete_shader(vert_shader);
+            gl.delete_shader(frag_shader);
         };
 
-        Ok(Self(program))
+        Ok(Self(gl, program))
     }
 
     pub fn use_shader(&self) {
-        unsafe { gl::UseProgram(self.0) };
+        unsafe { self.0.use_program(Some(self.1)) };
     }
 
-    pub fn get_id(&self) -> u32 {
-        self.0
+    pub fn get_shader(&self) -> glow::NativeProgram {
+        self.1
     }
 
-    pub fn get_uniform_location(&self, uniform_name: &str) -> Option<i32> {
-        let binding = std::ffi::CString::new(uniform_name).expect(concat!(
-            "CString::new failed in `get_uniform_location`",
-        ));
-
-        let location = unsafe { gl::GetUniformLocation(self.0, binding.as_ptr() as _) };
-
-        (location != -1).then_some(location)
+    pub fn get_uniform_location(&self, uniform_name: &str) -> Option<glow::UniformLocation> {
+        unsafe { self.0.get_uniform_location(self.1, uniform_name) }
     }
-
-    uniform!(set_bool, bool, Uniform1i);
-    uniform!(set_int, i32, Uniform1i);
-    uniform!(set_float, f32, Uniform1f);
 }
 
-impl Drop for Shader {
+impl<'a> Drop for Shader<'a> {
     fn drop(&mut self) {
-        unsafe { gl::DeleteProgram(self.0) };
+        unsafe { self.0.delete_program(self.1) };
     }
 }
 
@@ -121,35 +99,18 @@ fn parse_shader(input: &str) -> std::collections::HashMap<String, String> {
     map
 }
 
-fn compile_shader(ty: u32, source: &str) -> u32 {
-    let id = unsafe { gl::CreateShader(ty) };
+fn compile_shader(gl: &glow::Context, ty: u32, source: &str) -> glow::NativeShader {
+    let id = unsafe { gl.create_shader(ty).unwrap() };
 
     unsafe {
-        gl::ShaderSource(
-            id,
-            1,
-            &(source as *const str as *const i8),
-            [source.len() as _].as_ptr(),
-        );
-
-        gl::CompileShader(id);
+        gl.shader_source(id, source);
+        gl.compile_shader(id);
 
         let mut result: i32 = 0;
-        gl::GetShaderiv(id, gl::COMPILE_STATUS, &raw mut result);
+        let info = gl.get_shader_info_log(id);
 
-        if result == gl::FALSE as _ {
-            let mut length: i32 = 0;
-            gl::GetShaderiv(id, gl::INFO_LOG_LENGTH, &raw mut length);
-            let mut message = vec![0; length as _];
-            gl::GetShaderInfoLog(id, length, &raw mut length, message.as_mut_ptr() as *mut i8);
-
-            println!(
-                "Failed to compile shader!\n{}",
-                std::str::from_utf8(message.as_slice()).unwrap()
-            );
-
-            gl::DeleteShader(id);
-            return 0;
+        if !info.is_empty() {
+            eprintln!("[Info] `compile_shader(...)`: {info}");
         }
     }
 
